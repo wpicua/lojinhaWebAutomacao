f4eea138c40e653b02fcb3369940f3c210ecb5ad
Wellington Silva <wpicua05@hotmail.com>
2022-08-09 10:09:14 -0300

    Inicializando o repositorio com os arquivos iniciais do projeto

---
+++ src/test/java/modulos/produtos/ProdutosTest.java
@@ -0,0 +1,118 @@
+package modulos.produtos;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import paginas.LoginPage;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Set;
+
+@DisplayName("Testes WEB do Modulo de Produtos")
+
+public class ProdutosTest {
+
+    private WebDriver navegador;
+
+    @BeforeEach
+    public void beforeEach() {
+        // Abrir navegador
+        System.setProperty("webdriver.chrome.driver", "D:\\Cursos\\Programa de Testes e Qualidade de Software\\Drivers\\chromedriver_win32\\chromedriver.exe");
+        this.navegador = new ChromeDriver();
+
+        // Vou maximizar a tela
+        this.navegador.manage().window().maximize();
+
+        // Vou definir um tempo de espera padrao de 5 segundos
+        this.navegador.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        // Navegar para a pagina da lojinha Web
+        this.navegador.get("http://165.227.93.41/lojinha-web/v2/");
+    }
+
+    @Test
+    @DisplayName("Nao e permitido registrar um produto com valor igual a zero")
+    public void testNaoEPermitidoRegistrarProdutoComValorigualAZero() {
+
+        String mensagemApresentada = new LoginPage(navegador)
+                .informarOUsuario("admin")
+                .informarASenha("admin")
+                .submeterFormularioDeLogin()
+                .acessarFormularioAdicaoNovoProduto()
+                .informarNomeDoProduto("Mackbook Pro")
+                .informarValorDoProduto("000")
+                .informarAsCoresDoProduto("branco, preto")
+                .submeterFormularioDeAdicaoComErro()
+                .capturarMensagemApresentada();
+
+        Assertions.assertEquals("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00", mensagemApresentada);
+
+    }
+
+    @Test
+    @DisplayName("Nao e permitido registrar um produto com valor maior que 7000")
+    public void testNaoEPermitidoRegistrarProdutoComValorAcimaDeSeteMil() {
+
+        String mensagemApresentada = new LoginPage(navegador)
+                .informarOUsuario("admin")
+                .informarASenha("admin")
+                .submeterFormularioDeLogin()
+                .acessarFormularioAdicaoNovoProduto()
+                .informarNomeDoProduto("Computador Dell")
+                .informarValorDoProduto("700001")
+                .informarAsCoresDoProduto("branco, preto")
+                .submeterFormularioDeAdicaoComErro()
+                .capturarMensagemApresentada();
+
+        Assertions.assertEquals("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00", mensagemApresentada);
+
+    }
+
+    @Test
+    @DisplayName("Posso adicionar produtos que estejam na faixa 0,01 a 7000,00 reais")
+    public void testPossoAdicionarProdutosComValorDeUmCentavoASeteMilReais() {
+
+        String mensagemApresentada = new LoginPage(navegador)
+                .informarOUsuario("admin")
+                .informarASenha("admin")
+                .submeterFormularioDeLogin()
+                .acessarFormularioAdicaoNovoProduto()
+                .informarNomeDoProduto("Computador Itautec")
+                .informarValorDoProduto("300000")
+                .informarAsCoresDoProduto("branco, azul")
+                .submeterFormularioDeAdicaoComSucesso()
+                .capturarMensagemApresentada();
+
+        Assertions.assertEquals("Produto adicionado com sucesso", mensagemApresentada);
+
+    }
+
+    @Test
+    @DisplayName("Posso adicionar produtos no valor de 7.000,00 reais")
+    public void testPossoAdicionarProdutosComValorDeSeteMilReais() {
+
+        String mensagemApresentada = new LoginPage(navegador)
+                .informarOUsuario("admin")
+                .informarASenha("admin")
+                .submeterFormularioDeLogin()
+                .acessarFormularioAdicaoNovoProduto()
+                .informarNomeDoProduto("Computador Itautec")
+                .informarValorDoProduto("700000")
+                .informarAsCoresDoProduto("branco, azul")
+                .submeterFormularioDeAdicaoComSucesso()
+                .capturarMensagemApresentada();
+
+        Assertions.assertEquals("Produto adicionado com sucesso", mensagemApresentada);
+
+    }
+
+
+    @AfterEach
+    public void afterEach() {
+        // Vou fechar o navegador
+        navegador.quit();
+    }
+}
